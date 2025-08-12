5c5bc8ae6f15636b58aca27b0e7741f3614eb344
Queemsyyy <rus.korotkikh.06@bk.ru>
2025-08-12 21:48:37 +0300

    Delete Stock Up - Online shop directory

--- Stock Up - Online shop/script/script.js
+++
@@ -1,6 +0,0 @@
-let burger = document.querySelector(".burger")
-let menu = document.querySelector(".burger-menu")
-
-burger.addEventListener('click', function(toggle){
-    burger.classList.add("active")
-})
