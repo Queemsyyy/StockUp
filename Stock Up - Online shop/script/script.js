242f9d34fb18c423b59b5b2a629773f9f37942aa
Queemsyyy <rus.korotkikh.06@bk.ru>
2025-08-12 21:42:28 +0300

    Add files via upload

---
+++ Stock Up - Online shop/script/script.js
@@ -0,0 +1,6 @@
+let burger = document.querySelector(".burger")
+let menu = document.querySelector(".burger-menu")
+
+burger.addEventListener('click', function(toggle){
+    burger.classList.add("active")
+})
